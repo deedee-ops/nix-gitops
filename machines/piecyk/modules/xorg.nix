17b2238e6a1df22c9ec7934af5a3072ffb67cafd
Igor Rzegocki <igor@rzegocki.pl>
2024-07-15 12:23:05 +0200

    fix(piecyk): reduce GPU power usage

--- machines/piecyk/modules/xorg.nix
+++ machines/piecyk/modules/xorg.nix
@@ -4,6 +4,7 @@
     Option         "ForceFullCompositionPipeline" "on"
     Option         "AllowIndirectGLXProtocol" "off"
     Option         "TripleBuffer" "on"
+    Option         "UseNvKmsCompositionPipeline" "false"
     Option         "metamodes" "DP-2: 3840x2160_120 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: 3840x2160_120 +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}"
   '';
 }
