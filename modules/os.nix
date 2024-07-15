c2da83aaa5b4c8414164b9bdce754488e8a1453d
Igor Rzegocki <igor@rzegocki.pl>
2024-07-15 20:33:57 +0200

    feat: use proper swap partition on all machines

--- modules/os.nix
+++ modules/os.nix
@@ -3,12 +3,6 @@
   # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # swapfile
-  swapDevices = [{
-    device = "/swapfile";
-    size = 4096; # 4GB
-  }];
-
   # save power
   powerManagement.cpuFreqGovernor = "powersave";
 
