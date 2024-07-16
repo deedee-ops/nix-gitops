bb0691b08a7df028676630e22e8899671fdc8bbd
Igor Rzegocki <igor@rzegocki.pl>
2024-07-16 22:01:09 +0200

    fix(piecyk): remove untrusted interface, as VLAN200 is now native

--- machines/piecyk/networking.nix
+++ machines/piecyk/networking.nix
@@ -7,7 +7,6 @@
     vlans = {
       mgmt0 = { id = 42; interface = "enp5s0"; };
       trst0 = { id = 100; interface = "enp5s0"; };
-      untrst0 = { id = 200; interface = "enp5s0"; };
       iot0 = { id = 210; interface = "enp5s0"; };
     };
     interfaces.iot0.ipv4.addresses = [{
