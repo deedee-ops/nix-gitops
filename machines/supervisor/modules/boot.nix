2da6016a7c6d861a551efa2a54e4464d7d791970
Igor Rzegocki <igor@rzegocki.pl>
2024-08-08 19:12:10 +0200

    fix(supervisor): add missing iptables_nat module to fix wg-easy

--- machines/supervisor/modules/boot.nix
+++ machines/supervisor/modules/boot.nix
@@ -4,4 +4,7 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+
+  # necessary for backwards compatibility with alpine and (by extension) wg-easy
+  boot.kernelModules = [ "iptable_nat" ];
 }
